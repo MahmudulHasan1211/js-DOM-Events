56f28d43c2d615b8b5f996754183c92b2093393a
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-16 15:51:36 +0600

    most used way to use addEventListener on a button

--- Dom events.js
+++ Dom events.js
@@ -5,3 +5,7 @@ colorChange.onclick = makeYellow;
 function makeYellow (){
     document.body.style.backgroundColor = 'yellow';
 }
+
+document.getElementById('gold').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
